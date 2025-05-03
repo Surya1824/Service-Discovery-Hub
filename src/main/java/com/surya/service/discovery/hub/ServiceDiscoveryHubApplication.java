1a87cbf467761b035a0ba3ad58c90186b4b6171c
Surya1824 <suryateja5347@gmail.com>
2025-05-03 16:59:07 +0530

    Configured Discovery Server

--- src/main/java/com/surya/service/discovery/hub/ServiceDiscoveryHubApplication.java
+++ src/main/java/com/surya/service/discovery/hub/ServiceDiscoveryHubApplication.java
@@ -2,8 +2,10 @@ package com.surya.service.discovery.hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryHubApplication {
 
 	public static void main(String[] args) {
