9793ea76a4bbdfc269270758113789944f178043
Surya1824 <suryateja5347@gmail.com>
2025-05-03 15:30:30 +0530

    First Commit

---
+++ src/main/java/com/surya/service/discovery/hub/ServiceDiscoveryHubApplication.java
@@ -0,0 +1,13 @@
+package com.surya.service.discovery.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surya/service/discovery/hub/ServiceDiscoveryHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surya.service.discovery.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
